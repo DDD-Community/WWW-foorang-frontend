8745bb5a453a8c3614e87c4a25c24acf0c30deb7
김도국 <dgkim3811@naver.com>
2022-05-26 22:11:00 +0900

    react-query, navigation 초기 세팅

---
+++ src/navigation/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Test from 'components/Test'
+
+const MainStack = createNativeStackNavigator()
+
+export const Main = () => (
+  <MainStack.Navigator>
+    <MainStack.Screen name="test" component={Test} />
+  </MainStack.Navigator>
+)
